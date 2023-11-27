2787bcf294f3160280ed99a683f0ee6d58d3843f
Jose Ernesto Garcia <josernestogarcia609@gmail.com>
2023-11-27 09:34:04 -0300

    202311/27

--- src/routes/index.route.js
+++ src/routes/index.route.js
@@ -1,10 +1,12 @@
 import { Router}  from 'express'
 import turns from './turns.route.js'
 import mails from './mails.route.js'
+import test from './test.route.js'
 
 const router = Router()
 
 router.use('/turns', turns)
 router.use('/mails', mails)
+router.use('/test', test);
 
 export default router;
